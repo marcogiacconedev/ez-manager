4099fa786848bec668e5d05cc730a62077b0a777
Marco Giaccone <marco.giaccone.dev@gmail.com>
2025-12-30 23:07:41 +0100

    creata prima tabella nel db

--- database-dump/db-dump.sql
+++ database-dump/db-dump.sql
@@ -1,1 +1,8 @@
-
+-- USER
+CREATE TABLE public."User" (
+	id uuid NOT NULL,
+	email varchar NOT NULL,
+	name varchar NOT NULL,
+	"password" varchar NOT NULL,
+	CONSTRAINT user_pk PRIMARY KEY (id)
+);
